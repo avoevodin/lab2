973858b7e128fd596f67c23dae471ba1b2312891
Andrey Voevodin <avoevodin8888@gmail.com>
2020-12-12 19:09:06 +0300

    added convertions to more than 10-base numbers

--- convert_digit.py
+++ convert_digit.py
@@ -1,20 +1,35 @@
 # Converting number in dec to the selected number base
 
+import string
+
+alph = string.ascii_lowercase
+print(alph)
+
 print('Input number in dec')
 x = int(input())
-print('Input number base')
-base = int(input())
+
+i = 0
+base = 0
+while i < 3 and (base == 0 or base > 26):
+    print('Input number base, is less than 26 required')
+    base = int(input())
 
 y_revers = ''
 y = ''
 
 while x > 0:
-    y_revers += str(x % base)
+    rem = x % base
+    if rem > 9:
+        rem = alph[rem - 9]
+    else:
+        rem = str(rem)
+    
+    y_revers += rem
     x //= base
   
 for i in range(len(y_revers) - 1, -1, -1):
     y += y_revers[i] 
     
-print(y, end = '\n')
-print(y, end = '')
+# print(y, end = '\n') # shifting down after end of printing
+# print(y, end = '') # doing nothing after end of printing
 print(y)
