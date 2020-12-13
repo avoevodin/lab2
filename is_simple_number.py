1a9589fe3dda843ad0be19b84e7ebaacfdb7ee34
Andrey Voevodin <avoevodin8888@gmail.com>
2020-12-14 00:23:38 +0300

    added is_simple_number.py

--- is_simple_number.py
+++ is_simple_number.py
@@ -12,10 +12,39 @@ def is_simple_number(x):
         is_simple = x % divisor != 0
         divisor += 1
     return is_simple
+
+def factorize_number(x):
+    """Factorize number to it's multiplicators.
+       Print these multiplicators in a row with divisor ' * '.
     
+    """
+    divisor = 2
+    res = []
+    res_str = ''
+    while x > 1:
+        if x % divisor == 0:
+            res.append(divisor)
+            x //= divisor
+        else:
+            divisor += 1
+    else: 
+        res.append(1)
+    
+    first_step = 1
+    for i in res:
+        if first_step:
+            sep = ''
+            first_step = 0
+        else:
+            sep = ' * '
+        res_str += sep + str(i) 
+    print(res_str)           
+        
 print('Input positive int number:')
 x = int(input())
 if is_simple_number(x) == True:
     print("The number is simple")
 else:
     print("The number isn't simple")
+    
+factorize_number(x)
