50a173e503d603a4ea108261d79edd88143d3d39
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-01 23:54:40 +0300

    completed task 19

--- robot-tasks-master/task_19.py
+++ robot-tasks-master/task_19.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    pass
+    action = move_left
+    while wall_is_above() and not wall_is_on_the_right():
+        action()
+        if wall_is_on_the_left():
+            action = move_right
+    jailbreak_is_ok = not wall_is_above()
+    while not wall_is_above():
+        move_up()
+    if jailbreak_is_ok:
+        while not wall_is_on_the_left():
+            move_left()
 
 
 if __name__ == '__main__':
