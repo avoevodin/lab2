ee784f21105f09cf7a453f018294e91aa98b34c6
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-01 23:54:35 +0300

    completed task 18

--- robot-tasks-master/task_18.py
+++ robot-tasks-master/task_18.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
+    action = move_left
+    while wall_is_above():
+        action()
+        if wall_is_on_the_left():
+            action = move_right
+    while not wall_is_above():
+        move_up()
+    while not wall_is_on_the_left():
+        move_left()
+
 
 
 if __name__ == '__main__':
