2849dc9f6a04f6cd5527f3e7db6e262a096d424d
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-01 22:35:30 +0300

    completed task 6

--- robot-tasks-master/task_6.py
+++ robot-tasks-master/task_6.py
@@ -5,7 +5,10 @@ from pyrob.api import *
 
 @task
 def task_5_3():
-    pass
+    wall_started = wall_is_beneath()
+    while not wall_started or wall_is_beneath():
+        wall_started = wall_started if wall_started else wall_is_beneath()
+        move_right()
 
 
 if __name__ == '__main__':
