2b8a6cffb2e06ed7d2bac3531641e8fc1069f134
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-01 23:01:15 +0300

    completed task 11

--- robot-tasks-master/task_11.py
+++ robot-tasks-master/task_11.py
@@ -5,7 +5,13 @@ from pyrob.api import *
 
 @task
 def task_8_4():
-    pass
+    while 1 == 1:
+        if wall_is_above() == wall_is_beneath() is True:
+            fill_cell()
+        if not wall_is_on_the_right():
+            move_right()
+        else:
+            break
 
 
 if __name__ == '__main__':
