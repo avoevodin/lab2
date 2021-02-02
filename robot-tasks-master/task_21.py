9e4a2611c46095b3a6cdc20f8980576d1f96c708
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-02 18:42:27 +0300

    completed task 21

--- robot-tasks-master/task_21.py
+++ robot-tasks-master/task_21.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
+    move_right()
+    for i in range(13):
+        move_down()
+        fill_cell()
+        j = 0
+        for j in range(i):
+            move_right()
+            fill_cell()
+        if i > 0:
+            move_left(j + 1)
+    move_down()
 
 
 if __name__ == '__main__':
