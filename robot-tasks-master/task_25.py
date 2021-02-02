ffd581fe65f578a088a0a893801a2c9ea4e8f0e4
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-02 18:42:46 +0300

    completed task 25

--- robot-tasks-master/task_25.py
+++ robot-tasks-master/task_25.py
@@ -2,10 +2,28 @@
 
 from pyrob.api import *
 
+def fill_the_cross():
+    move_right()
+    fill_cell()
+    move_down()
+    fill_cell()
+    move_left()
+    fill_cell()
+    move_right(2)
+    fill_cell()
+    move_left()
+    move_down()
+    fill_cell()
+    move_up(2)
+    move_left()
 
 @task
 def task_2_2():
-    pass
+    move_down()
+    for i in range(5):
+        fill_the_cross()
+        if i < 4:
+            move_right(4)
 
 
 if __name__ == '__main__':
