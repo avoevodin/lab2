e1135fe88b6d3b3e300d2bab15c6fa6c39f9899d
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-03 01:47:48 +0300

    completed task 28

--- robot-tasks-master/task_28.py
+++ robot-tasks-master/task_28.py
@@ -5,7 +5,10 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    filled_cells = 0
+    while filled_cells < 5:
+        move_right()
+        filled_cells += cell_is_filled()
 
 
 if __name__ == '__main__':
