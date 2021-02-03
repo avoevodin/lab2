40fc2f35137a5511b1c77f17f5eb064a5f36ec80
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-03 10:49:31 +0300

    completed task 32

--- robot-tasks-master/task_32.py
+++ robot-tasks-master/task_32.py
@@ -5,8 +5,22 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    pass
-
+    filled_cells_count = 0
+    while not wall_is_on_the_right():
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()
+        move_right()
+        if not wall_is_above() and not wall_is_beneath():
+            break
+        while not wall_is_above():
+            move_up()
+            if cell_is_filled():
+                filled_cells_count += 1
+            else:
+                fill_cell()
+        while not wall_is_beneath():
+            move_down()
+    mov('ax', filled_cells_count)
 
 if __name__ == '__main__':
     run_tasks()
